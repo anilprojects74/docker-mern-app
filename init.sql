7dc47008c1d53452ebaba88795efc437b555dc03
anilprojects74 <anilprojects74@gmail.com>
2025-05-14 12:55:36 +0000

    Composed docker file single command runs the site

---
+++ init.sql
@@ -0,0 +1,11 @@
+-- init.sql
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    confirmation_token VARCHAR(255),
+    is_confirmed TINYINT(1) DEFAULT 0
+);
+
